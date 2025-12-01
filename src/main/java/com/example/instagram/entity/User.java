c3469ebf59cadae381917ffc0b5d0839ccd01c4f
qqqkyj <123rkdduswn@naver.com>
2025-12-01 13:54:40 +0900

    User Modeling

--- src/main/java/com/example/instagram/entity/User.java
+++ src/main/java/com/example/instagram/entity/User.java
@@ -1,4 +1,33 @@
 package com.example.instagram.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter @Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true, length = 30)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private Role role;
+    @Column(nullable = false)
+    private String bio;
+
+    @Builder
+    public User(String username, String password, String email, Role role, String bio) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role != null ? role : Role.USER;
+        this.bio = bio;
+    }
 }

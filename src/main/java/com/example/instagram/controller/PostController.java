9e0f764e7a903413c54ba8ef7def78af2c732b9f
qqqkyj <123rkdduswn@naver.com>
2025-12-02 10:17:36 +0900

    게시물 생성 및 디비 저장 확인

--- src/main/java/com/example/instagram/controller/PostController.java
+++ src/main/java/com/example/instagram/controller/PostController.java
@@ -1,19 +1,46 @@
 package com.example.instagram.controller;
 
 import com.example.instagram.dto.request.PostCreateRequest;
+import com.example.instagram.security.CustomUserDetails;
+import com.example.instagram.service.PostService;
+import com.example.instagram.service.PostServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
     @GetMapping("/new")
     public String createForm(Model model) {
         model.addAttribute("postCreateRequest", new PostCreateRequest());
         return "post/form";
+    }
+
+    //현재 로그인한 사용자 정보 함께 넘기기
+    @PostMapping
+    public String create(@Valid @ModelAttribute PostCreateRequest postCreateRequest,
+                         BindingResult bindingResult,
+                         //세션을 통해 현재 로그인한 사용자 정보
+                         @AuthenticationPrincipal CustomUserDetails userDetails) {
+        if (bindingResult.hasErrors()) {
+            return "post/form";
+        }
+
+        postService.create(postCreateRequest, userDetails.getId());
+
+        return "redirect:/";
     }
 }
